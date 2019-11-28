5abe0084eeb6c1323e2d80f09cee23187384ee66
usuryasatyakumar <usuryasatyakumar@gmail.com>
2019-11-28 17:20:42 +0530

    service registry

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -2,6 +2,7 @@ package io.pivotal.pal.tracker.restsupport;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -12,9 +13,11 @@ import org.springframework.web.client.RestTemplate;
 public class RestConfig {
 
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
+
 
     @Bean
     public ObjectMapper objectMapper() {
