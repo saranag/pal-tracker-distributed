e231c334241ee6c3f036711a532923be467d6249
Uppuluri, Surya <surya_uppuluri@dell.com>
2019-12-02 07:50:49 +0530

    manifest add

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -13,7 +13,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableEurekaClient
-@ComponentScan("io.pivotal.pal.tracker.restsupport")
+@ComponentScan({"io.pivotal.pal.tracker.allocations","io.pivotal.pal.tracker.restsupport"})
 public class App {
 
     public static void main(String[] args) {
